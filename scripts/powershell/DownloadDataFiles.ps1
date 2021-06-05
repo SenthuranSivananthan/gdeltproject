d7e2a64e8a30c465d05abb017f96e60ad60c597b
Senthuran Sivananthan <senthuran.sivananthan@microsoft.com>
2021-06-05 10:05:36 -0400

    Create DownloadDataFiles.ps1

---
+++ scripts/powershell/DownloadDataFiles.ps1
@@ -0,0 +1,56 @@
+function DownloadData($limitToYears)
+{
+    $DATA_URL = "http://data.gdeltproject.org/gdeltv2/masterfilelist.txt"
+    $FILE_ON_DISK = "$pwd\masterfilelist.txt"
+
+    $RAW_DATA_BASEFOLDER = "$pwd\raw"
+
+    New-Item -Path $RAW_DATA_BASEFOLDER -ItemType Directory -Force
+
+    Write-Host "Downloading File: $DATA_URL to $FILE_ON_DISK"
+    Invoke-WebRequest -Uri $DATA_URL -OutFile $FILE_ON_DISK
+
+    Write-Host "Reading File: $FILE_ON_DISK"
+    $streamReader = New-Object System.IO.StreamReader($FILE_ON_DISK)
+
+    $recordCount = 0
+
+    while (($line = $streamReader.ReadLine()) -ne $null)
+    {
+        $recordCount++
+        $lineData = $line -split ' '
+    
+        $downloadUrl = $lineData[2].Trim()
+        $fileName = $downloadUrl.Substring($downloadUrl.LastIndexOf("/") + 1)
+        $year = $fileName.Substring(0, 4)
+
+        if ($limitToYears.Length -eq 0 -or $limitToYears -contains $year)
+        {
+            # Ensure sub directory is created
+            if (-not(Test-Path -Path $RAW_DATA_BASEFOLDER\$year))
+            {
+                New-Item -Path $RAW_DATA_BASEFOLDER\$year -ItemType Directory -Force
+            }
+
+            $downloadSaveLocation = "$RAW_DATA_BASEFOLDER\$year\$fileName"
+
+            if (-not(Test-Path -Path $downloadSaveLocation))
+            {
+                Write-Host "Downloading: $downloadUrl"
+                Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadSaveLocation
+            }
+            else
+            {
+                Write-Host "Exists.  Skipped $downloadUrl"
+            }
+        }
+        else
+        {
+            Write-Host "Skipped, file not in included years: $limitToYears.  Skipped $downloadUrl"
+        }
+    }
+}
+
+$limitYears = 2019, 2020
+#$limitYears = $()
+DownloadData $limitYears
